724ade265fe7647abc37d7ea3beba6b1602807c3
Skyecubus <64573700+Skyecubus@users.noreply.github.com>
2025-05-18 13:15:57 -0400

    0.0.1
    first iteration of petal bot

---
+++ src/main.rs
@@ -0,0 +1,160 @@
+use anyhow::Context as _;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use shuttle_runtime::SecretStore;
+use tracing::{error, info};
+use regex::Regex;
+
+struct Bot;
+
+struct MessageCounter;
+
+impl TypeMapKey for MessageCounter{
+    type Value = i32;
+}
+
+//async fn restartCount(ctx: &Context){
+    //let mut data = ctx.data.write().await;
+    //let counter = data.get_mut::<MessageCounter>().unwrap();
+    //*counter = 0;
+//}
+
+//increments the counter
+async fn countUP(ctx: &Context){
+    let mut data = ctx.data.write().await;
+    let counter = data.get_mut::<MessageCounter>().unwrap();
+    *counter += 1;
+    //debug remove later 
+    println!("{}", counter);
+
+}
+
+//checks if counter = 10
+async fn checkCount(ctx: &Context) -> bool {
+    let mut data = ctx.data.write().await;
+    let counter = data.get_mut::<MessageCounter>().unwrap();
+
+    if *counter == 100{
+        *counter = 0;
+        true
+    }else{
+        false
+    }
+}
+
+
+//some call words are: 
+//good floret, !ping, !hello, !gay, !yay!, !trans rights, !trans wrongs
+#[async_trait]
+impl EventHandler for Bot {
+    async fn message(&self, ctx: Context, msg: Message) {
+
+        countUP(&ctx).await;
+
+        if msg.author.bot == false{
+
+             if checkCount(&ctx).await{
+            self.respond(ctx, msg, "100!").await;
+            }
+        
+
+            else if Regex::new(r".*good floret.*").expect("reason").captures(&msg.content.to_lowercase()).is_some() {
+                self.respond(ctx, msg, "get florted bitch").await;
+            }
+
+            //oh no
+            else if Regex::new(r".*o+h+ n+o+.**").expect("reason").captures(&msg.content.to_lowercase()).is_some() {
+                self.respond(ctx, msg, "oh no! mistress we need xenodruggies stat!").await;
+            }
+
+            else if Regex::new(r".*good floret.*").expect("reason").captures(&msg.content.to_lowercase()).is_some() {
+                self.respond(ctx, msg, "get florted bitch").await;
+            }
+
+            else if msg.content.to_lowercase() == "!ping"{
+                println!("Shard {}", ctx.shard_id);
+
+                self.respond(ctx, msg, "pong!").await;
+            }
+
+            
+            else if msg.content.to_lowercase() == "!hello"{
+                self.respond(ctx, msg, "world!").await;
+            }
+            
+
+            else if msg.content.to_lowercase() == "!gay" {
+                self.respond(ctx, msg, "Im the gayest little girl!").await;
+            }
+
+
+            else if Regex::new(r".*yay!.*").expect("reason").captures(&msg.content.to_lowercase()).is_some() {
+                self.respond(ctx, msg, "Yaaaaaaaaaaay,,,,,,,,!!!!!!🏳️‍⚧️").await;
+            }
+
+
+            else if msg.content.to_lowercase() == "!trans rights"{
+                self.respond(ctx, msg, "Trans Rights!!! 🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️🏳️‍⚧️").await;
+            }
+
+            else if msg.content.to_lowercase() == "!trans wrongs"{
+                self.respond(ctx, msg, "Trans Wrongs!!! 💙🩷🤍🩷💙").await;
+            }
+
+        }
+
+       
+
+
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name);
+    }
+}
+
+
+impl Bot{
+    async fn respond(&self, ctx: Context, msg: Message, response: &str){
+        if let Err(e) = msg.channel_id.say(&ctx.http, response).await {
+            error!("Error sending message: {:?}", e);
+        }
+        
+    }
+
+}
+
+
+#[shuttle_runtime::main]
+async fn serenity(
+    #[shuttle_runtime::Secrets] secrets: SecretStore,
+) -> shuttle_serenity::ShuttleSerenity {
+    // Get the discord token set in `Secrets.toml`
+    let token = secrets
+        .get("DISCORD_TOKEN")
+        .context("'DISCORD_TOKEN' was not found")?;
+
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Bot)
+        .await
+        .expect("Err creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<MessageCounter>(0);
+    }
+
+    if let Err(e) = client.start_shards(2). await {
+        println!("Client error: {e:?}");
+    }
+
+
+    Ok(client.into())
+}
+
+
